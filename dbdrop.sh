61df3c8d94c51440c60780dedc676d0051d20912
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2016-09-02 14:03:32 +0200

    drop script param check

--- dbdrop.sh
+++ dbdrop.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# small helper to drop all tables from a give db
+# small helper script to drop all tables from a given db
 DB=$1
-
+if [ -n "${DB+1}" ]; then
 while true; do
-    read -p "Do you really want to drop all tables from ${DB} [yn]?" yn
+    read -p "Do you really want to drop all tables from ${DB} [yn] ? " yn
     case $yn in
         [Yy]* ) break;;
-        [Nn]* ) exit;;
+        [Nn]* ) echo "Exiting ..."; exit;;
         * ) echo "Please answer yes [Yy] or no [Nn].";;
     esac
 done
+else
+  echo "Usage: ./dbdrop <db>"
+  exit
+fi
 
 PGDB="-h localhost -W -U hostview ${DB}"
 TABLES=`psql $PGDB -t --command "SELECT string_agg(table_name, ',') FROM information_schema.tables WHERE table_schema='public'"`
 
-echo dropping tables:${TABLES}
+echo "Dropping tables:${TABLES}"
 
 psql $PGDB --command "DROP TABLE IF EXISTS ${TABLES} CASCADE"
