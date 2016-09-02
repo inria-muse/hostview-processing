da0ae4beec2d79a84efb61f40efbffd191124770
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2016-09-02 13:55:17 +0200

    drop script y/n

--- dbdrop.sh
+++ dbdrop.sh
@@ -3,12 +3,18 @@
 # small helper to drop all tables from a give db
 DB=$1
 
-echo dropping tables from ${DB}
+while true; do
+    read -p "Do you really want to drop all tables from ${DB} [yn]?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes [Yy] or no [Nn].";;
+    esac
+done
 
 PGDB="-h localhost -W -U hostview ${DB}"
-
 TABLES=`psql $PGDB -t --command "SELECT string_agg(table_name, ',') FROM information_schema.tables WHERE table_schema='public'"`
 
 echo dropping tables:${TABLES}
 
-#psql $PGDB --command "DROP TABLE IF EXISTS ${TABLES} CASCADE"
+psql $PGDB --command "DROP TABLE IF EXISTS ${TABLES} CASCADE"
